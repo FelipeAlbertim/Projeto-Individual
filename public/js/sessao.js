cef164d9de30dc47555ab8594bd8ebbfc99956e7
FelipeAlbertim <felipe.albertim@sptech.school>
2024-05-17 16:15:05 -0300

    Ultima verção ainda com erro de fazer cadastro com api...

--- public/js/sessao.js
+++ public/js/sessao.js
@@ -19,18 +19,16 @@ function limparSessao() {
 
 // carregamento (loading)
 function aguardar() {
-    var divAguardar = document.getElementById("div_aguardar");
-    divAguardar.style.display = "flex";
+    alert('CARREGANDO....')
+    // var divAguardar = document.getElementById("div_aguardar");
+    // divAguardar.style.display = "flex";
 }
 
 function finalizarAguardar(texto) {
-    var divAguardar = document.getElementById("div_aguardar");
-    divAguardar.style.display = "none";
+    alert("FINALIZANDO AGUARDAR")
 
-    var divErrosLogin = document.getElementById("div_erros_login");
-    if (texto) {
-        divErrosLogin.style.display = "flex";
-        divErrosLogin.innerHTML = texto;
+    if (texto == true) {
+        alert(`Erro: ${texto}`)
     }
 }
 
--- src/controllers/usuarioController.js
+++ src/controllers/usuarioController.js
@@ -59,18 +59,19 @@ function cadastrar(req, res) {
     var senha = req.body.senhaServer
     var historia = req.body.historiaServer
 
+    console.log("chegou na controller", req)
+
 
     // Faça as validações dos valores
-    if (nome == undefined) {
-        res.status(400).send("Seu nome está undefined!");
-    } else if (email == undefined) {
+    if (nomeUsuario == undefined) {
+        res.status(400).send("Seu nome de usuario está undefined!");
+    } else if (email){
         res.status(400).send("Seu email está undefined!");
-    } else if (senha == undefined) {
-        res.status(400).send("Sua senha está undefined!");
-    } else if (empresaId == undefined) {
-        res.status(400).send("Sua empresa está undefined!");
-    } else {
-
+    } else if (senha){
+        res.status(400).send("Seu senha está undefined!");
+    }
+    
+    else {
         // Passe os valores como parâmetro e vá para o arquivo usuarioModel.js
         usuarioModel.cadastrar(nomeUsuario,nomeReal,email,senha,historia)
             .then(
