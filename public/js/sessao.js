f2851e2c2928eafd12bca2f1f0002648863ced8f
FelipeAlbertim <felipe.albertim@sptech.school>
2024-05-22 23:42:59 -0300

    Inicio do desenvolvimento do questionario

--- public/js/sessao.js
+++ public/js/sessao.js
@@ -3,11 +3,13 @@ function validarSessao() {
     var email = sessionStorage.EMAIL_USUARIO;
     var nome = sessionStorage.NOME_USUARIO;
 
-    var b_usuario = document.getElementById("b_usuario");
+    // var b_usuario = document.getElementById("b_usuario");
 
     if (email != null && nome != null) {
-        b_usuario.innerHTML = nome;
+        // b_usuario.innerHTML = nome;
+        window.location = "../questionario.html";
     } else {
+        alert("Login Obrigatório !")        
         window.location = "../login.html";
     }
 }
